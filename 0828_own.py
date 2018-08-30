7725ef28fa07ec6ac4ed27222a8512477a12fda9
nopro <320029098@qq.com>
2018-08-30 15:42:56 +0800

    完整的多模型测试代码和voting hard/soft代码

---
+++ 0828_own.py
@@ -0,0 +1,212 @@
+# titanic问题的一个模型处理，小数据集上跑了大量的模型，极有参考价值
+
+
+
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import svm, tree, linear_model, neighbors, naive_bayes, ensemble, discriminant_analysis, gaussian_process
+from xgboost import XGBClassifier
+
+#Common Model Helpers
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn import feature_selection
+from sklearn import model_selection
+from sklearn import metrics
+
+#Visualization
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
+import seaborn as sns
+from pandas.tools.plotting import scatter_matrix
+
+#Configure Visualization Defaults
+#%matplotlib inline = show plots in Jupyter Notebook browser
+%matplotlib inline
+mpl.style.use('ggplot')
+sns.set_style('white')
+pylab.rcParams['figure.figsize'] = 12,8
+
+
+
+root_path = 'C:/Users/32002/Desktop/noprocode/titanic/all'
+
+data_raw = pd.read_csv('%s/%s' % (root_path, 'train.csv'))
+data_val = pd.read_csv('%s/%s' % (root_path, 'test.csv'))
+
+
+data1 = data_raw.copy(deep = True)
+data_cleaner = [data1,data_val]
+for dataset in data_cleaner:
+    dataset['Age'].fillna(dataset['Age'].median(),inplace = True)
+    dataset['Embarked'].fillna(dataset['Embarked'].mode()[0], inplace = True)
+    dataset['Fare'].fillna(dataset['Fare'].median(), inplace = True)
+
+drop_column = ['PassengerId','Cabin','Ticket']
+data1.drop(drop_column,axis = 1,inplace  = True)
+for dataset in data_cleaner:
+    dataset['FamilySize'] = dataset['SibSp']+dataset['Parch']+1
+    dataset['IsAlone'] = 1
+    dataset['IsAlone'].loc[dataset['FamilySize'] > 1] = 0
+
+    dataset['Title']  = dataset['Name'].str.split(', ',expand = True)[1].\
+                            str.split(".",expand = True)[0]
+
+    dataset['FareBin'] = pd.qcut(dataset['Fare'],4)
+
+    dataset['AgeBin'] = pd.cut(dataset['Age'].astype(int),5)
+
+stat_min = 10
+title_names = (data1['Title'].value_counts()<stat_min)
+
+data1['Title'] = data1['Title'].apply(lambda x: 'Misc' if title_names[x] == True else x)
+
+label = LabelEncoder()
+
+for dataset in data_cleaner:
+    dataset['Sex_Code'] = label.fit_transform(dataset['Sex'])
+    dataset['Embarked_Code'] = label.fit_transform(dataset['Embarked'])
+    dataset['Title_Code'] = label.fit_transform(dataset['Title'])
+    dataset['AgeBin_Code'] = label.fit_transform(dataset['AgeBin'])
+    dataset['FareBin_Code'] = label.fit_transform(dataset['FareBin'])
+
+Target = ['Survived']
+
+data1_x = ['Sex','Pclass', 'Embarked', 'Title','SibSp', 'Parch', 'Age', 'Fare', 'FamilySize', 'IsAlone'] #pretty name/values for charts
+data1_x_calc = ['Sex_Code','Pclass', 'Embarked_Code', 'Title_Code','SibSp', 'Parch', 'Age', 'Fare'] #coded for algorithm calculation
+data1_xy =  Target + data1_x
+print('Original X Y: ', data1_xy, '\n')
+
+
+#define x variables for original w/bin features to remove continuous variables
+data1_x_bin = ['Sex_Code','Pclass', 'Embarked_Code', 'Title_Code', 'FamilySize', 'AgeBin_Code', 'FareBin_Code']
+data1_xy_bin = Target + data1_x_bin
+print('Bin X Y: ', data1_xy_bin, '\n')
+
+
+#define x and y variables for dummy features original
+data1_dummy = pd.get_dummies(data1[data1_x])
+data1_x_dummy = data1_dummy.columns.tolist()
+data1_xy_dummy = Target + data1_x_dummy
+print('Dummy X Y: ', data1_xy_dummy, '\n')
+
+train1_x, test1_x, train1_y, test1_y = model_selection.train_test_split(data1[data1_x_calc], data1[Target], random_state = 0)
+train1_x_bin, test1_x_bin, train1_y_bin, test1_y_bin = model_selection.train_test_split(data1[data1_x_bin], data1[Target] , random_state = 0)
+train1_x_dummy, test1_x_dummy, train1_y_dummy, test1_y_dummy = model_selection.train_test_split(data1_dummy[data1_x_dummy], data1[Target], random_state = 0)
+
+
+print("Data1 Shape: {}".format(data1.shape))
+print("Train1 Shape: {}".format(train1_x.shape))
+print("Test1 Shape: {}".format(test1_x.shape))
+
+
+
+MLA = [
+    #Ensemble Methods
+    ensemble.AdaBoostClassifier(),
+    ensemble.BaggingClassifier(),
+    ensemble.ExtraTreesClassifier(),
+    ensemble.GradientBoostingClassifier(),
+    ensemble.RandomForestClassifier(),
+
+    #Gaussian Processes
+    gaussian_process.GaussianProcessClassifier(),
+    
+    #GLM
+    linear_model.LogisticRegressionCV(),
+    linear_model.PassiveAggressiveClassifier(),
+    linear_model.RidgeClassifierCV(),
+    linear_model.SGDClassifier(),
+    linear_model.Perceptron(),
+    
+    #Navies Bayes
+    naive_bayes.BernoulliNB(),
+    naive_bayes.GaussianNB(),
+    
+    #Nearest Neighbor
+    neighbors.KNeighborsClassifier(),
+    
+    #SVM
+    svm.SVC(probability=True),
+    svm.NuSVC(probability=True),
+    svm.LinearSVC(),
+    
+    #Trees    
+    tree.DecisionTreeClassifier(),
+    tree.ExtraTreeClassifier(),
+    
+    #Discriminant Analysis
+    discriminant_analysis.LinearDiscriminantAnalysis(),
+    discriminant_analysis.QuadraticDiscriminantAnalysis(),
+
+    
+    #xgboost: http://xgboost.readthedocs.io/en/latest/model.html
+    XGBClassifier()    
+    ]
+
+cv_split = model_selection.ShuffleSplit(n_splits = 10, test_size = .3, train_size = .6, random_state = 0 ) # run model 10x with 60/30 split intentionally leaving out 10%
+
+#create table to compare MLA metrics
+MLA_columns = ['MLA Name', 'MLA Parameters','MLA Train Accuracy Mean', 'MLA Test Accuracy Mean', 'MLA Test Accuracy 3*STD' ,'MLA Time']
+MLA_compare = pd.DataFrame(columns = MLA_columns)
+
+MLA_predict = data1[Target]
+
+#index through MLA and save performance to table
+row_index = 0
+
+for alg in MLA:
+
+    #set name and parameters
+    MLA_name = alg.__class__.__name__
+    MLA_compare.loc[row_index, 'MLA Name'] = MLA_name
+    MLA_compare.loc[row_index, 'MLA Parameters'] = str(alg.get_params())
+    
+    #score model with cross validation: http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.cross_validate.html#sklearn.model_selection.cross_validate
+    cv_results = model_selection.cross_validate(alg, data1[data1_x_bin], data1[Target], cv  = cv_split)
+
+    MLA_compare.loc[row_index, 'MLA Time'] = cv_results['fit_time'].mean()
+    MLA_compare.loc[row_index, 'MLA Train Accuracy Mean'] = cv_results['train_score'].mean()
+    MLA_compare.loc[row_index, 'MLA Test Accuracy Mean'] = cv_results['test_score'].mean()   
+    #if this is a non-bias random sample, then +/-3 standard deviations (std) from the mean, should statistically capture 99.7% of the subsets
+    MLA_compare.loc[row_index, 'MLA Test Accuracy 3*STD'] = cv_results['test_score'].std()*3   #let's know the worst that can happen!
+    
+
+    #save MLA predictions - see section 6 for usage
+    alg.fit(data1[data1_x_bin], data1[Target])
+    MLA_predict[MLA_name] = alg.predict(data1[data1_x_bin])
+    
+    row_index+=1
+MLA_compare.sort_values(by = ['MLA Test Accuracy Mean'], ascending = False, inplace = True)
+MLA_compare
+
+
+
+# 参数调整部分
+
+# 可视化tree
+# import graphviz 
+# dot_data = tree.export_graphviz(dtree, out_file=None, 
+#                                 feature_names = data1_x_bin, class_names = True,
+#                                 filled = True, rounded = True)
+# graph = graphviz.Source(dot_data) 
+# graph
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
